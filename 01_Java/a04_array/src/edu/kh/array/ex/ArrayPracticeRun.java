a401e03c116814ff0a16604374ab03f5a9d64c4b
kkm990708 <kkm990708@gmail.com>
2023-08-11 17:36:14 +0900

    08/11 끝

--- 01_Java/a04_array/src/edu/kh/array/ex/ArrayPracticeRun.java
+++ 01_Java/a04_array/src/edu/kh/array/ex/ArrayPracticeRun.java
@@ -3,7 +3,7 @@ package edu.kh.array.ex;
 public class ArrayPracticeRun {
 public static void main(String[] args) {
 	ArrayPractice practice = new ArrayPractice();
-//	practice.BingoGame();
-	practice.practice14();
+	practice.BingoGame();
+//	practice.practice14();
 }
 }
