c5af726c4e3f4e385000a6daf0da03ad94ef12f4
kkm990708 <kkm990708@gmail.com>
2023-10-19 17:49:02 +0900

    10/19 커밋

--- 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/admin/service/AdminService.java
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/admin/service/AdminService.java
@@ -1,5 +1,7 @@
 package edu.kh.project.admin.service;
 
+import java.util.List;
+
 import edu.kh.project.member.model.dto.Member;
 
 public interface AdminService {
@@ -9,5 +11,35 @@ public interface AdminService {
 	 * @return
 	 */
 	Member selectMember(String inputEmail);
+
+	/** 전체 회원 조회
+	 * @return memberList
+	 */
+	List<Member> selectAll();
+
+	/** 전체 회원 조회(+정렬)
+	 * @param sort
+	 * @return memberList
+	 */
+	List<Member> selectSort(int sort);
+
+	/** 회원 탈퇴 복구 
+	 * @param memberNo
+	 * @return result
+	 */
+	int restoration(int memberNo);
+
+	/** 권한 변경
+	 * @param memberNo
+	 * @param authority 
+	 * @return
+	 */
+	int changeAuthority(int memberNo);
+
+	/** 비밀번호 초기화
+	 * @param memberNo
+	 * @return
+	 */
+	int initPw(int memberNo);
 
 }
--- 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/admin/service/AdminServiceImpl.java
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/admin/service/AdminServiceImpl.java
@@ -1,6 +1,11 @@
 package edu.kh.project.admin.service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,9 +20,57 @@ public class AdminServiceImpl implements AdminService{
 	@Autowired
 	private AdminMapper mapper;
 	
+	@Autowired
+	private BCryptPasswordEncoder bcrypt;
+	
 	@Override 
 	public Member selectMember(String inputEmail) {
 		
 		return mapper.selectMember(inputEmail); 
 	}
+	
+	@Override
+	public List<Member> selectAll() {
+		
+		return mapper.selectAll();
+	}
+	
+	@Override
+	public List<Member> selectSort(int sort) {
+		
+		// sort 값에 따라 가공 처리
+		String query = null;
+		
+		switch (sort) {
+		case 1: query= "MEMBER_NO DESC"; break;
+		case 2: query= "MEMBER_NO"; break;
+		case 3: query= "MEMBER_EMAIL DESC"; break;
+		case 4: query= "MEMBER_EMAIL"; break;
+		}
+		
+		return mapper.selectSort(query);
+	}
+	
+	
+	@Override
+	public int restoration(int memberNo) {
+		return mapper.restoration(memberNo);
+	}
+	
+	@Override
+	public int changeAuthority(int memberNo) {
+		return mapper.changeAuthority(memberNo);
+	}
+	
+	
+	@Override
+	public int initPw(int memberNo) {
+		
+		Map<String, Object> map = new HashMap<>();
+		map.put("memberPw", bcrypt.encode("1234")) ;
+		map.put("memberNo", memberNo);
+		
+		return mapper.initPw(map);
+	}
+	
 }
