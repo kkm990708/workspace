2938cf43fffc5e4008bbf59ebd6627dba844b1c1
kkm990708 <kkm990708@gmail.com>
2023-10-27 17:47:23 +0900

    10/27 끝

--- 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/board/model/service/BoardService.java
+++ 06_Framework/SpringBoot/boardProject-boot/src/main/java/edu/kh/project/board/model/service/BoardService.java
@@ -2,8 +2,21 @@ package edu.kh.project.board.model.service;
 
 import java.util.Map;
 
+import edu.kh.project.board.model.dto.Board;
+
 public interface BoardService {
 
+	/** 게시글 목록 조회
+	 * @param boardCode
+	 * @param cp
+	 * @return
+	 */
 	Map<String, Object> selectBoardList(int boardCode, int cp);
+
+	/** 게시글 상세 조회
+	 * @param map
+	 * @return
+	 */
+	Board boardDetail(Map<String, Object> map);
 
 }
